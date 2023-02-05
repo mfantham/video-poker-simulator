7318b8d6d09e71257b871fdb6d3b9a6663309762
Marcus <mfantham@oni.bio>
2023-02-04 19:41:56 -0800

    volume 0 mutes

--- src/audio/SynthEngine.tsx
+++ src/audio/SynthEngine.tsx
@@ -1,12 +1,15 @@
 import { useCallback, useEffect, useRef } from "react";
 import { sleep } from "../utils/sleep";
+import { useVolume } from "../redux/hooks";
 
 export const SynthEngine = () => {
   const audioContext = useRef<AudioContext | null>(new AudioContext());
+  const volume = useVolume();
 
   const dealCard = useCallback(() => {
     const audioCtx = audioContext.current;
-    if (!audioCtx) return;
+    if (!audioCtx || volume === 0) return;
+    console.log(volume);
     const gain = audioCtx.createGain();
     gain.connect(audioCtx.destination);
 
@@ -25,47 +28,51 @@ export const SynthEngine = () => {
     const source = audioCtx.createBufferSource();
     source.buffer = noiseBuffer;
     source.connect(audioCtx.destination);
-    gain.gain.setTargetAtTime(0.2, audioCtx.currentTime, 0.01);
+    gain.gain.setTargetAtTime(volume * 0.2, audioCtx.currentTime, 0.01);
     gain.gain.setTargetAtTime(0, audioCtx.currentTime, 0.015);
 
     source.start();
     source.stop(audioCtx.currentTime + 0.03);
-  }, []);
+  }, [volume]);
 
   const winOnTheDeal = useCallback(() => {
     const audioCtx = audioContext.current;
-    if (!audioCtx) return;
+    if (!audioCtx || volume === 0) return;
     const osc = audioCtx.createOscillator();
     const gain = audioCtx.createGain();
     osc.connect(gain);
     gain.connect(audioCtx.destination);
 
     osc.frequency.value = 1000;
-    gain.gain.setTargetAtTime(0.2, audioCtx.currentTime, 0.01);
+    gain.gain.setTargetAtTime(volume * 0.2, audioCtx.currentTime, 0.01);
     gain.gain.setTargetAtTime(0, audioCtx.currentTime, 0.02);
 
     osc.start();
     osc.stop(audioCtx.currentTime + 0.3);
-  }, []);
+  }, [volume]);
 
-  const payOut = useCallback(async (e: Event) => {
-    const n = (e as CustomEvent).detail;
-    const audioCtx = audioContext.current;
-    if (!audioCtx) return;
-    const osc = audioCtx.createOscillator();
-    const gain = audioCtx.createGain();
-    osc.connect(gain);
-    gain.connect(audioCtx.destination);
+  const payOut = useCallback(
+    async (e: Event) => {
+      const n = (e as CustomEvent).detail;
+      const audioCtx = audioContext.current;
+      if (!audioCtx || volume === 0) return;
+      const osc = audioCtx.createOscillator();
+      const gain = audioCtx.createGain();
+      gain.gain.setValueAtTime(volume * 0.5, audioCtx.currentTime);
+      osc.connect(gain);
+      gain.connect(audioCtx.destination);
 
-    osc.start();
-    for (let i = 0; i < n; i++) {
-      const noteNumber = i % 24;
-      osc.frequency.value = 200 * Math.pow(2, noteNumber / 12);
-      await sleep(100);
-    }
+      osc.start();
+      for (let i = 0; i < n; i++) {
+        const noteNumber = i % 24;
+        osc.frequency.value = 200 * Math.pow(2, noteNumber / 12);
+        await sleep(100);
+      }
 
-    osc.stop();
-  }, []);
+      osc.stop();
+    },
+    [volume]
+  );
 
   useEffect(() => {
     window.addEventListener("deal-card", dealCard);
--- src/types/variant.ts
+++ src/types/variant.ts
@@ -1,5 +1,4 @@
 export enum VARIANT {
-  DUMB_LUCK = "Dumb luck",
   JACKS_OR_BETTER = "Jacks or better",
   DEUCES_WILD = "Deuces wild",
 }
